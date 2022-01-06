479a8a64fca774ae9cdd3189030e8ab8c09e7419
FlutterBoyfriend <formatable2018@gmail.com>
2022-01-06 02:06:15 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Portifolio',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //  home:
+    );
+  }
+}
